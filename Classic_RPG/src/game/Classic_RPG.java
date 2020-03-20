35039fa38d0fb5862fe85a0a91371a11cd461e21
LavergneC <lavergne.clement@yahoo.fr>
2020-03-20 16:40:57 +0100

    base project

---
+++ Classic_RPG/src/game/Classic_RPG.java
@@ -0,0 +1,9 @@
+package game;
+
+public class Classic_RPG {
+
+	public static void main(String[] args) {
+		System.out.println("This is Classic RPG game; enjoy !");
+	}
+
+}
---
+++ Classic_RPG/src/module-info.java
@@ -0,0 +1,2 @@
+module classic_RPG {
+}
