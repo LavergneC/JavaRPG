1069d519f2fcee09de75b6270c015ee68729baa1
nicolas_gibaud <n.gibaud@catie.fr>
2020-03-26 16:47:13 +0100

    Add Wave class

---
+++ Classic_RPG/src/game/Wave.java
@@ -0,0 +1,12 @@
+package game;
+import java.util.ArrayList;
+
+public class Wave {
+	private int number;
+	private ArrayList<Monster> monsters;
+	
+	public Wave(int number_, ArrayList<Monster> list_monster) {
+		number = number_;
+		monsters = list_monster;
+	}
+}
